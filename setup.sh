0301592bda62275623b7cc1562291435f3d5e558
Jong-Chan Kim <jongchank@gmail.com>
2025-09-14 20:17:08 +0900

    Reorder the package list

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ cmd.exe /c winget source update
 cmd.exe /c winget install Google.Chrome --silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install Dropbox.Dropbox --silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install Git.Git --silent --accept-package-agreements --accept-source-agreements
+cmd.exe /c winget install Docker.DockerDesktop --silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install Microsoft.PowerToys --silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install Microsoft.WindowsTerminal --silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install Microsoft.VisualStudioCode --silent --accept-package-agreements --accept-source-agreements
@@ -11,4 +12,3 @@ cmd.exe /c winget install SlackTechnologies.Slack --silent --accept-package-agre
 cmd.exe /c winget install Kakao.KakaoTalk--silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install Notion.Notion --silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install Python.Python.3.13 --silent --accept-package-agreements --accept-source-agreements
-cmd.exe /c winget install Docker.DockerDesktop --silent --accept-package-agreements --accept-source-agreements
