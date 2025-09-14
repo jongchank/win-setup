500110a4bf415a13914b468a3c24e7bf2118b26e
Jong-Chan Kim <jongchank@gmail.com>
2025-09-14 18:32:07 +0900

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -5,7 +5,7 @@ winget install Microsoft.PowerToys --silent --accept-package-agreements --accept
 winget install Microsoft.WindowsTerminal --silent --accept-package-agreements --accept-source-agreements
 winget install Microsoft.VisualStudioCode --silent --accept-package-agreements --accept-source-agreements
 winget install Dropbox.Dropbox --silent --accept-package-agreements --accept-source-agreements
-winget install 9MZ95KL8MR0L --silent --accept-package-agreements --accept-source-agreements # snip and sketch
+winget install 9MZ95KL8MR0L --silent --accept-package-agreements --accept-source-agreements &:: snip and sketch
 winget install Git.Git --silent --accept-package-agreements --accept-source-agreements
 winget install SlackTechnologies.Slack --silent --accept-package-agreements --accept-source-agreements
 winget install Kakao.KakaoTalk--silent --accept-package-agreements --accept-source-agreements
