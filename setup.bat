52b12aa72b806b3087a385f4992dadf419e5d7a0
Jong-Chan Kim <jongchank@gmail.com>
2025-09-14 20:10:26 +0900

    Reorder the package list

--- setup.bat
+++ setup.bat
@@ -1,12 +1,11 @@
 winget source update
 winget install Google.Chrome --silent --accept-package-agreements --accept-source-agreements
 winget install Dropbox.Dropbox --silent --accept-package-agreements --accept-source-agreements
+winget install Git.Git --silent --accept-package-agreements --accept-source-agreements
 winget install Microsoft.PowerToys --silent --accept-package-agreements --accept-source-agreements
 winget install Microsoft.WindowsTerminal --silent --accept-package-agreements --accept-source-agreements
 winget install Microsoft.VisualStudioCode --silent --accept-package-agreements --accept-source-agreements
-winget install Dropbox.Dropbox --silent --accept-package-agreements --accept-source-agreements
 winget install 9MZ95KL8MR0L --silent --accept-package-agreements --accept-source-agreements &:: snip and sketch
-winget install Git.Git --silent --accept-package-agreements --accept-source-agreements
 winget install SlackTechnologies.Slack --silent --accept-package-agreements --accept-source-agreements
 winget install Kakao.KakaoTalk--silent --accept-package-agreements --accept-source-agreements
 winget install Notion.Notion --silent --accept-package-agreements --accept-source-agreements
--- setup.sh
+++ setup.sh
@@ -2,12 +2,11 @@
 cmd.exe /c winget source update
 cmd.exe /c winget install Google.Chrome --silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install Dropbox.Dropbox --silent --accept-package-agreements --accept-source-agreements
+cmd.exe /c winget install Git.Git --silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install Microsoft.PowerToys --silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install Microsoft.WindowsTerminal --silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install Microsoft.VisualStudioCode --silent --accept-package-agreements --accept-source-agreements
-cmd.exe /c winget install Dropbox.Dropbox --silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install 9MZ95KL8MR0L --silent --accept-package-agreements --accept-source-agreements # snip and sketch
-cmd.exe /c winget install Git.Git --silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install SlackTechnologies.Slack --silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install Kakao.KakaoTalk--silent --accept-package-agreements --accept-source-agreements
 cmd.exe /c winget install Notion.Notion --silent --accept-package-agreements --accept-source-agreements
