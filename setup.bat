3614d750edf10b570d74a0345da3063d1b4762e1
Jong-Chan Kim <jongchank@users.noreply.github.com>
2025-09-19 17:59:14 +0900

    Sync setup.bat to setup.sh

--- setup.bat
+++ setup.bat
@@ -2,6 +2,7 @@ winget source update
 winget install Google.Chrome --silent --accept-package-agreements --accept-source-agreements
 winget install Dropbox.Dropbox --silent --accept-package-agreements --accept-source-agreements
 winget install Git.Git --silent --accept-package-agreements --accept-source-agreements
+winget install Docker.DockerDesktop --silent --accept-package-agreements --accept-source-agreements
 winget install Microsoft.PowerToys --silent --accept-package-agreements --accept-source-agreements
 winget install Microsoft.WindowsTerminal --silent --accept-package-agreements --accept-source-agreements
 winget install Microsoft.VisualStudioCode --silent --accept-package-agreements --accept-source-agreements
@@ -10,4 +11,3 @@ winget install SlackTechnologies.Slack --silent --accept-package-agreements --ac
 winget install Kakao.KakaoTalk--silent --accept-package-agreements --accept-source-agreements
 winget install Notion.Notion --silent --accept-package-agreements --accept-source-agreements
 winget install Python.Python.3.13 --silent --accept-package-agreements --accept-source-agreements
-winget install Docker.DockerDesktop --silent --accept-package-agreements --accept-source-agreements
