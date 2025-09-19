38303c6075c2a85dd734b19c64240f411c64a088
Jong-Chan Kim <jongchank@users.noreply.github.com>
2025-09-19 22:10:47 +0900

    Reorder and add installations in setup.bat

--- setup.bat
+++ setup.bat
@@ -1,15 +1,15 @@
 winget source update
 winget install vim.vim --silent --accept-package-agreements --accept-source-agreements
+winget install Git.Git --silent --accept-package-agreements --accept-source-agreements
+winget install Microsoft.WindowsTerminal --silent --accept-package-agreements --accept-source-agreements
 winget install Google.Chrome --silent --accept-package-agreements --accept-source-agreements
 winget install Dropbox.Dropbox --silent --accept-package-agreements --accept-source-agreements
-winget install Git.Git --silent --accept-package-agreements --accept-source-agreements
-winget install GitHub.GitHubDesktop --silent --accept-package-agreements --accept-source-agreements
-winget install Docker.DockerDesktop --silent --accept-package-agreements --accept-source-agreements
-winget install Microsoft.PowerToys --silent --accept-package-agreements --accept-source-agreements
-winget install Microsoft.WindowsTerminal --silent --accept-package-agreements --accept-source-agreements
 winget install Microsoft.VisualStudioCode --silent --accept-package-agreements --accept-source-agreements
 winget install 9MZ95KL8MR0L --silent --accept-package-agreements --accept-source-agreements &:: snip and sketch
+winget install Microsoft.PowerToys --silent --accept-package-agreements --accept-source-agreements
 winget install SlackTechnologies.Slack --silent --accept-package-agreements --accept-source-agreements
 winget install Kakao.KakaoTalk --silent --accept-package-agreements --accept-source-agreements
 winget install Notion.Notion --silent --accept-package-agreements --accept-source-agreements
 winget install Python.Python.3.13 --silent --accept-package-agreements --accept-source-agreements
+winget install GitHub.GitHubDesktop --silent --accept-package-agreements --accept-source-agreements
+winget install Docker.DockerDesktop --silent --accept-package-agreements --accept-source-agreements
